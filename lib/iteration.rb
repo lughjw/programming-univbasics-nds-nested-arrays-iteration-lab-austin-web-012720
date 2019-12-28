3340709fc31c2ea41f77aaf35aebc9c7c2d97bb1
Lugh Wilson <lughjw@gmail.com>
2019-12-28 01:46:05 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -43,7 +43,7 @@ def total_even_pairs(src)
   total = 0
   
   for i in 0...src.length do
-    if src[i][0]%2 == src[i][1]%2 == 0
+    if src[i][0] % 2 == src[i][1] % 2 == 0
       total+= src[i][0] + src[i][1]
     end
   end
